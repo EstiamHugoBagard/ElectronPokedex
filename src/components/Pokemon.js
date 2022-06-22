a89f19ef7acfda154fbacd0252bc1adea9e7931b
BagardHugo <56017385+BagardHugo@users.noreply.github.com>
2022-06-22 17:23:55 +0200

    Add and remove to team

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './styles/App.css';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import { CardActions } from '@mui/material';
 
 export const Pokemons = () => {
 
     const [pokemons, setPokemon] = React.useState([]);
     const [pokemonDetails, setPokemonDetails] = React.useState([]);
+    const [myTeam, setMyTeam] = React.useState([]);
 
     React.useEffect(() => {
         GetPokemon('https://pokeapi.co/api/v2/pokemon');
@@ -24,7 +25,6 @@ export const Pokemons = () => {
             .then(response => {
                 response.json()
                     .then(jsonResult => {
-                        console.log(jsonResult)
                         setPokemon(jsonResult)
                     })
             })
@@ -36,7 +36,6 @@ export const Pokemons = () => {
                 response.json()
                     .then(jsonResult => {
                         setPokemonDetails(jsonResult)
-                        console.log(jsonResult)
                     })
             })
     }
@@ -51,122 +50,191 @@ export const Pokemons = () => {
         return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     }
 
+    const loadStorage = () => {
+        const team = JSON.parse(localStorage.getItem('my-Team')) || []
+        setMyTeam(team);
+    }
+
+    const saveStorage = () => {
+        localStorage.setItem('my-Team', JSON.stringify(myTeam))
+    }
+
+    const addToTeam = (pokemon) => {
+        myTeam.push(pokemon)
+        saveStorage()
+        loadStorage()
+    }
+
+    const removeToTeam = (pokemonToRemove) => {
+        console.log(pokemonToRemove)
+        let pokemonIndex = myTeam.findIndex(x => x.name === pokemonToRemove.name);
+        console.log(pokemonIndex)
+        if (pokemonIndex !== -1) {
+            myTeam.splice(pokemonIndex, 1);
+        }
+
+        saveStorage()
+        loadStorage()
+    }
+
     if (pokemons.length !== 0) return (
         <div>
+            <h1>My pokemon team</h1>
+            {                
+                myTeam.map((poke, index) => {
+                    return (
+                            <img id={index} src={GetPokemonImage(poke.url)}></img>
+
+                    )
+                })
+            }
             <Grid container spacing={3}>
                 <Grid item xs={6}>
-                    <h2>Pokemon list</h2>
-                    <div className='Pokemons_container'>
-                        <Box sx={{ flexGrow: 1 }}>
-                            <Grid container spacing={3}>
-                                {
-                                    pokemons.results?.map((pokemon, index) => {
-                                        return (
-                                            <Grid onClick={() => {
-                                                GetPokemonDetails(`https://pokeapi.co/api/v2/pokemon/` + GetPokemonId(pokemon.url))
-                                            }}
-                                                id={index} item xs={8} md={5} xl={4}>
-                                                <Card sx={{ maxWidth: 345 }}>
-                                                    <CardMedia
-                                                        component="img"
-                                                        alt={pokemon.name}
-                                                        image={GetPokemonImage(pokemon.url)}
-                                                    />
-                                                    <CardContent>
-                                                        <Typography gutterBottom variant="h5" component="div">
-                                                            {pokemon.name}
-                                                        </Typography>
-                                                    </CardContent>
-                                                    <CardActions>
-                                                        <Button size="small">Détails</Button>
-                                                    </CardActions>
-                                                </Card>
+                    <Typography style={{ textAlign: 'center' }} gutterBottom variant="h2" component="div">
+                        Pokemon list
+                    </Typography>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={3}>
+                            {
+                                pokemons.results?.map((pokemon, index) => {
+                                    return (
+                                        <Grid onClick={() => {
+                                            GetPokemonDetails(`https://pokeapi.co/api/v2/pokemon/` + GetPokemonId(pokemon.url))
+                                        }}
+                                            id={index} item xs={8} md={5} xl={4}>
+                                            <Card className="card" sx={{ maxWidth: 345 }}>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    #{GetPokemonId(pokemon.url)}
+                                                </Typography>
+                                                <CardMedia
+                                                    component="img"
+                                                    alt={pokemon.name}
+                                                    image={GetPokemonImage(pokemon.url)}
+                                                />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="h5" component="div">
+                                                        {pokemon.name}
+                                                    </Typography>
+                                                </CardContent>
+                                                <CardActions>
+                                                    {
+                                                        myTeam.find(x => x.name === pokemon.name) ?
+                                                            <Button onClick={() => {
+                                                                removeToTeam(pokemon)
+                                                            }}
+                                                                size="small">Remove to team</Button>
+                                                            :
+                                                            <Button onClick={() => {
+                                                                addToTeam(pokemon)
+                                                            }}
+                                                                size="small">Add to team</Button>
+                                                    }
 
-                                            </Grid>
-                                        )
-                                    })
-                                }
-                            </Grid>
-                        </Box>
+                                                </CardActions>
+                                            </Card>
+
+                                        </Grid>
+                                    )
+                                })
+                            }
+                        </Grid>
+                    </Box>
+
+                    <div className="container_nav_buttons">
+                        {
+                            pokemons?.previous ?
+                                <Button
+                                    onClick={() => {
+                                        GetPokemon(pokemons.previous)
+                                    }}
+                                    variant="outlined">Previous</Button>
+                                :
+                                <Button
+                                    disabled
+                                    onClick={() => {
+                                        GetPokemon(pokemons.previous)
+                                    }}
+                                    variant="outlined">Previous</Button>
+                        }
+                        {
+                            pokemons?.next ?
+                                <Button
+                                    onClick={() => {
+                                        GetPokemon(pokemons.next)
+                                    }}
+                                    variant="outlined">Next</Button>
+                                :
+                                <Button
+                                    disabled
+                                    onClick={() => {
+                                        GetPokemon(pokemons.next)
+                                    }}
+                                    variant="outlined">Next</Button>
+                        }
                     </div>
                 </Grid>
                 <Grid item xs={6}>
-                    <h1>Pokemon details</h1>
                     {
                         pokemonDetails ?
-                            <Card>
+                            <Card style={{ position: 'fixed', width: '50%' }}>
+                                <Typography style={{ textAlign: 'center' }} gutterBottom variant="h2" component="div">
+                                    Pokemon details
+                                </Typography>
+                                <Typography style={{ textAlign: 'center' }} gutterBottom variant="h3" component="div">
+                                    ID : {pokemonDetails.id} {pokemonDetails.name}
+                                </Typography>
                                 <Grid container justifyContent="center" spacing={3}>
                                     <Grid item xs={4}>
+                                        <Typography style={{ textAlign: 'center' }} variant="h4">
+                                            Normal
+                                        </Typography>
                                         <CardMedia
                                             component="img"
                                             alt={pokemonDetails.name}
                                             image={pokemonDetails?.sprites?.front_default}
                                         />
-                                        <h4>Normal</h4>
                                     </Grid>
                                     <Grid item xs={4}>
-
+                                        <Typography style={{ textAlign: 'center' }} variant="h4">
+                                            Shiny
+                                        </Typography>
                                         <CardMedia
+                                            xs={2}
                                             component="img"
                                             alt={pokemonDetails.name}
                                             image={pokemonDetails.sprites?.front_shiny}
                                         />
-                                        <h4>Shiny</h4>
-
-
                                     </Grid>
                                 </Grid>
-                                <Typography gutterBottom variant="h4" component="div">
-                                    {pokemonDetails.name}
-                                </Typography>
-                                <h3>Types :</h3>
-                                {
-                                    pokemonDetails?.types?.map((type, index) => {
-                                        return (
-                                            <p id={index}>{type.type.name}</p>
-                                        )
-                                    })
-                                }
-                            </Card>
+                                <Grid container justifyContent="center" spacing={3}>
+                                    <Grid item xs={4}>
+                                        <h3>Types :</h3>
+                                        {
+                                            pokemonDetails?.types?.map((type, index) => {
+                                                return (
+                                                    <p id={index}>{type.type.name}</p>
+                                                )
+                                            })
+                                        }
+                                    </Grid>
 
+                                    <Grid item xs={4}>
+                                        <h3>Abilities :</h3>
+                                        {
+                                            pokemonDetails?.abilities?.map((ability, index) => {
+                                                return (
+                                                    <p id={index}>{ability.ability.name}</p>
+                                                )
+                                            })
+                                        }
+                                    </Grid>
+                                </Grid>
+                            </Card>
                             :
                             null
                     }
                 </Grid>
             </Grid>
-
-
-
-            {
-                pokemons?.previous ?
-                    <Button
-                        onClick={() => {
-                            GetPokemon(pokemons.previous)
-                        }}
-                        variant="outlined">Previous</Button>
-                    :
-                    <Button
-                        disabled
-                        onClick={() => {
-                            GetPokemon(pokemons.previous)
-                        }}
-                        variant="outlined">Previous</Button>
-            }
-            {
-                pokemons?.next ?
-                    <Button
-                        onClick={() => {
-                            GetPokemon(pokemons.next)
-                        }}
-                        variant="outlined">Next</Button>
-                    :
-                    <Button
-                        disabled
-                        onClick={() => {
-                            GetPokemon(pokemons.next)
-                        }}
-                        variant="outlined">Next</Button>
-            }
         </div >
     )
 }
